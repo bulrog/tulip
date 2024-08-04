c29387b47c8ac472a6803b95de526ca5cd640827
Jerome Morel <jem@melexis.com>
2024-08-05 00:13:31 +0200

    trick to get keyboard and show what I type in the other field

--- app/src/main/java/com/example/tulip/MainActivity.kt
+++ app/src/main/java/com/example/tulip/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.tulip
 
+import android.content.Context
 import android.content.pm.ActivityInfo
 import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
+import android.text.TextWatcher
+import android.view.KeyEvent
+import android.view.WindowManager
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
 import java.util.*
 
 
@@ -13,8 +20,9 @@ class MainActivity : AppCompatActivity() {
 
     var timer: Timer? = null
     var timerTask: TimerTask? = null
-    var promptText: TextView? = null
+    lateinit var promptText: TextView
     var promptShow = true
+
     // TODO: handler is deprecated so to review:
     val handler: Handler = Handler()
 
@@ -36,18 +44,27 @@ class MainActivity : AppCompatActivity() {
         timer = null
     }
 
+
     fun initializeTimerTask() {
         timerTask = object : TimerTask() {
 
             override fun run() {
                 handler.post {
-                    val oldText = promptText?.text
-                    if (promptShow) {
+                    findViewById<EditText>(R.id.editTextTextPersonName4).apply {
+                        if (this.requestFocus()) {
 
-                        promptText?.text = "${oldText}_"
+                            val imm = getSystemService(InputMethodManager::class.java)
+                            imm.showSoftInput(this, InputMethodManager.SHOW_IMPLICIT)
+                        }
+
+                    }
+
+                    val oldText = promptText.text
+                    if (promptShow) {
+                        promptText.text = oldText?.dropLast(1)
                         promptShow = false
                     } else {
-                        promptText?.text = oldText?.dropLast(1)
+                        promptText.text = "${oldText}_"
                         promptShow = true
                     }
                 }
@@ -66,7 +83,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.console)
         promptText = findViewById<TextView>(R.id.consolePrompt).apply {
             setTextColor(Color.GREEN)
-            text = " this is some text"
+            text = ""
+        }
+
+        findViewById<EditText>(R.id.editTextTextPersonName4).apply {
+            setText("")
+            doOnTextChanged { text, start, before, count ->
+                if (promptShow) {
+                    promptText.text = "${text}_"
+                } else promptText.text = text
+            }
         }
         startTimer()
 
