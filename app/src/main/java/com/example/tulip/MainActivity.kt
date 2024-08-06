3be9b39fab84fbeac2649c81c5e964622a6aac6d
Jerome Morel <jem@melexis.com>
2024-08-06 22:38:03 +0200

    add sound for the key strike and refactor the code

--- app/src/main/java/com/example/tulip/MainActivity.kt
+++ app/src/main/java/com/example/tulip/MainActivity.kt
@@ -1,13 +1,11 @@
 package com.example.tulip
 
-import android.content.Context
 import android.content.pm.ActivityInfo
 import android.graphics.Color
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
-import android.text.TextWatcher
-import android.view.KeyEvent
-import android.view.WindowManager
+import android.os.Looper
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.TextView
@@ -18,39 +16,18 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    var timer: Timer? = null
-    var timerTask: TimerTask? = null
+    lateinit var timer: Timer
     lateinit var promptText: TextView
-    var promptShow = true
+    var promptVisible = false
+    lateinit var mediaPlayer:MediaPlayer
 
-    // TODO: handler is deprecated so to review:
-    val handler: Handler = Handler()
-
-    fun startTimer() {
-        //set a new Timer
-        timer = Timer()
-
-        //initialize the TimerTask's job
-        initializeTimerTask()
-
-        //schedule the timer, after the first 5000ms the TimerTask will run every 10000ms
-        timer?.schedule(timerTask, 0, 500) //
-    }
-
-    // TODO: add when app not in focus and also when resume to restart the timer, See example: https://examples.javacodegeeks.com/android/core/activity/android-timertask-example/
-    fun stoptimertask() {
-        //stop the timer, if it's not already null
-        timer?.cancel()
-        timer = null
-    }
-
-
-    fun initializeTimerTask() {
-        timerTask = object : TimerTask() {
+    val handler: Handler = Handler(Looper.getMainLooper())
+    fun initializeTimerTask() :TimerTask{
+        return object : TimerTask() {
 
             override fun run() {
                 handler.post {
-                    findViewById<EditText>(R.id.editTextTextPersonName4).apply {
+                    findViewById<EditText>(R.id.input_text).apply {
                         if (this.requestFocus()) {
 
                             val imm = getSystemService(InputMethodManager::class.java)
@@ -60,12 +37,12 @@ class MainActivity : AppCompatActivity() {
                     }
 
                     val oldText = promptText.text
-                    if (promptShow) {
+                    if (promptVisible) {
                         promptText.text = oldText?.dropLast(1)
-                        promptShow = false
+                        promptVisible = false
                     } else {
                         promptText.text = "${oldText}_"
-                        promptShow = true
+                        promptVisible = true
                     }
                 }
             }
@@ -83,21 +60,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.console)
         promptText = findViewById<TextView>(R.id.consolePrompt).apply {
             setTextColor(Color.GREEN)
-            text = ""
+            text = ">"
         }
+        mediaPlayer=MediaPlayer.create(this,R.raw.keyboard_key_strike)
 
-        findViewById<EditText>(R.id.editTextTextPersonName4).apply {
+        findViewById<EditText>(R.id.input_text).apply {
             setText("")
             doOnTextChanged { text, start, before, count ->
-                if (promptShow) {
-                    promptText.text = "${text}_"
-                } else promptText.text = text
+
+                mediaPlayer.start()
+                if (promptVisible) {
+                    promptText.text = ">${text}_"
+                } else promptText.text = ">$text"
             }
         }
-        startTimer()
+        timer = Timer().apply {  schedule(initializeTimerTask(), 0, 500)}
 
 
     }
 
-
+    override fun onDestroy() {
+        super.onDestroy()
+        timer.cancel()
+    }
 }
